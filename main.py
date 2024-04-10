744aeaf8bd32d437b0ac4e540e285badd44f9c95
seamusmccol <seam.cco@gmail.com>
2024-04-10 12:31:18 -0400

    i added the encoder! hurrah!

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+
+def menu():
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print()
+
+
+if __name__ == "__main__":
+    menu()
+    greenfn = int(input("Enter an option: "))
+    if greenfn == 1:
+        password = str(input("password お願いいたします。"))
+        print("I stored, and encoded dat bih")
+
+
